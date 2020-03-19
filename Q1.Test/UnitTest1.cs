b35eefa5fb6cf3fa5cd6dc95b6aa521aac1cf4d3
Srinivasa <srigumm@hotmail.com>
2020-03-19 12:00:49 -0400

    chore(*): Add templates for dotnet core project
      - create empty projects and add to solution file

---
+++ Q1.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Q1.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
